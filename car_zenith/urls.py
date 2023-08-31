c30f5f2a1db3b860106bf2fc26a350b002802241
tanvirch0750 <tanvir0750@yahoo.com>
2023-09-01 01:22:15 +0600

    initialize car zenith project, create necessary apps

---
+++ car_zenith/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('account/', include('account.urls')),
+    path('brand/', include('brand.urls')),
+    path('car/', include('car.urls')),
+    path('orders/', include('orders.urls')),
+    path('cart/', include('cart.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
