924569d14e4882e055fc551d6c0b6f3fb8358b5b
tanvirch0750 <tanvir0750@yahoo.com>
2023-09-01 02:39:59 +0600

    created base html

--- car_zenith/urls.py
+++ car_zenith/urls.py
@@ -8,9 +8,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('account/', include('account.urls')),
-    path('brand/', include('brand.urls')),
-    path('car/', include('car.urls')),
-    path('orders/', include('orders.urls')),
-    path('cart/', include('cart.urls')),
+    # path('account/', include('account.urls')),
+    # path('brand/', include('brand.urls')),
+    # path('car/', include('car.urls')),
+    # path('orders/', include('orders.urls')),
+    # path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,4 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#db2d2e',
-          secondary: '#363636',
-          accent: '#67cba0',
-          neutral: '#999999',
-          'base-100': '#ffffff',
-          info: '#3abff8',
-          success: '#36d399',
-          warning: '#fbbd23',
-          error: '#f87272',
-        },
-      },
-    ],
-  },
   theme: {},
 };
