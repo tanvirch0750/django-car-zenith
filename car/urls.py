4c822ae9958959a13439b154a8d3c30d46470c2c
tanvirch0750 <tanvir0750@yahoo.com>
2023-09-02 00:09:59 +0600

    implemented all cars page

--- car/urls.py
+++ car/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
+    path('', views.cars, name='cars'),
     path('brand/<slug:brand_slug>/<slug:car_slug>/', views.car_detail, name='car_detail'),
 ]
--- car/views.py
+++ car/views.py
@@ -3,6 +3,15 @@ from car.models import Car
 
 # Create your views here.
 
+def cars(request):
+     cars = Car.objects.filter(is_available=True)
+
+     context = {
+        'cars': cars,
+      }
+     return render(request, 'car/cars.html', context)
+
+
 def car_detail(request, brand_slug, car_slug):
     try:
         single_car = Car.objects.get(brand__slug=brand_slug, slug=car_slug)
