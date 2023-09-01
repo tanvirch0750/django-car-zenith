3a010b780162d6ad5667385cab207bc89f2622f8
tanvirch0750 <tanvir0750@yahoo.com>
2023-09-01 21:22:42 +0600

    created car model

--- car/admin.py
+++ car/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'color', 'brand', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(models.Car, CarAdmin)
--- car/models.py
+++ car/models.py
@@ -1,2 +1,32 @@
 from django.db import models
+from django.db.models import Avg, Count
+from django.urls import reverse
+
+from account.models import Account
+from brand.models import Brand
+
+# Create your models here.
+
+class Car(models.Model):
+    title           = models.CharField(max_length=200, unique=True)
+    slug            = models.SlugField(max_length=200, unique=True)
+    description     = models.TextField(max_length=1000, blank=True)
+    price           = models.IntegerField()
+    images          = models.ImageField(upload_to='photos/cars')
+    color           = models.CharField(max_length=50)
+    model           = models.CharField(max_length=50)
+    is_available    = models.BooleanField(default=True)
+    brand           = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    features        = models.TextField(max_length=500, blank=True)
+    created_date    = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now=True)
+    is_featured     = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.brand.slug, self.slug])
+    
+
 
