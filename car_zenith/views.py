1929408005a2bee11768d53b4d88a75ecfc38f7c
tanvirch0750 <tanvir0750@yahoo.com>
2023-09-03 01:44:02 +0600

    change home.html directory

--- car_zenith/views.py
+++ car_zenith/views.py
@@ -4,4 +4,4 @@ from car.models import Car
 
 def home(request):
     featured_cars = Car.objects.filter(is_featured=True)
-    return render(request, 'home.html', {'cars': featured_cars})
+    return render(request, 'car/home.html', {'cars': featured_cars})
